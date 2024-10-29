a4428df9a6e32f1523469b28d5f492b64cd10ccd
Songjh0322 <happymine0322@naver.com>
2024-10-29 17:15:30 +0900

    add particle system

--- Assets/Scripts/ForwardMovement.cs
+++ Assets/Scripts/ForwardMovement.cs
@@ -8,7 +8,7 @@ public class ForwardMovement : MonoBehaviour
     public float speed = 100f;
     private int count = 0;
 
-
+    public GameObject explosionParticle;
     // Update is called once per frame
     void Update()
     {
@@ -18,11 +18,17 @@ public class ForwardMovement : MonoBehaviour
     // 적과 충돌 시 호출되는 메서드
     private void OnTriggerEnter(Collider other)
     {
-     
+
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             count++;  // count 증가
             Debug.Log("Enemy 격파!: " + count);
+
+            // 폭발 파티클 생성
+            if (explosionParticle != null)
+            {
+                Instantiate(explosionParticle, other.transform.position, Quaternion.identity);
+            }
 
             // 적 오브젝트 파괴
             Destroy(other.gameObject);
